84986a9e81e80203c746b30ba716d49dc6896969
Prettyboy69 <popoolamoses40@gsmil.com>
2024-11-24 17:21:58 +0100

    buld file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
-import Product from "./Page/Product";
-import Homepage from "./Page/Homepage";
-import Pricing from "./Page/Pricing";
-import PageNotFound from "./Page/PageNotFound";
-import AppLayout from "./Page/AppLayout";
-import Login from "./Page/Login";
-import CityList from "./Component/CityList";
-// import { useEffect, useState } from "react";
-import CountryList from "./Component/CountryList";
-import City from "./Component/City";
-import Form from "./Component/Form";
+import { lazy, Suspense } from "react";
 import { CitiesProvider } from "./Contexts/ContextCities";
 import { Authcontext } from "./Contexts/Authcontext";
 import ProtectedRoute from "./Page/ProtectedRoute";
+
+import CityList from "./Component/CityList";
+
+import CountryList from "./Component/CountryList";
+import City from "./Component/City";
+import Form from "./Component/Form";
+import SpinnerFullPage from "./Component/SpinnerFullPage";
+
+// import Product from "./Page/Product";
+// import Homepage from "./Page/Homepage";
+// import Pricing from "./Page/Pricing";
+// import PageNotFound from "./Page/PageNotFound";
+// import AppLayout from "./Page/AppLayout";
+// import Login from "./Page/Login";
+// import { useEffect, useState } from "react";
+const Homepage = lazy(() => import("./Page/Homepage"));
+const Product = lazy(() => import("./Page/Product"));
+const Pricing = lazy(() => import("./Page/Pricing"));
+const AppLayout = lazy(() => import("./Page/AppLayout"));
+const Login = lazy(() => import("./Page/Login"));
+const PageNotFound = lazy(() => import("./Page/PageNotFound"));
 
 const App = () => {
   return (
@@ -21,28 +31,30 @@ const App = () => {
       <Authcontext>
         <CitiesProvider>
           <BrowserRouter>
-            <Routes>
-              <Route index element={<Homepage />} />
-              <Route path="product" element={<Product />} />
-              <Route path="Pricing" element={<Pricing />} />
-              <Route path="Login" element={<Login />} />
-              <Route
-                path="app"
-                element={
-                  <ProtectedRoute>
-                    <AppLayout />
-                  </ProtectedRoute>
-                }
-              >
-                <Route index element={<Navigate replace to="cities" />} />
+            <Suspense fallback={<SpinnerFullPage />}>
+              <Routes>
+                <Route index element={<Homepage />} />
+                <Route path="product" element={<Product />} />
+                <Route path="Pricing" element={<Pricing />} />
+                <Route path="Login" element={<Login />} />
+                <Route
+                  path="app"
+                  element={
+                    <ProtectedRoute>
+                      <AppLayout />
+                    </ProtectedRoute>
+                  }
+                >
+                  <Route index element={<Navigate replace to="cities" />} />
 
-                <Route path="Cities" element={<CityList />} />
-                <Route path="cities/:id" element={<City />} />
-                <Route path="Countries" element={<CountryList />} />
-                <Route path="Form" element={<Form />} />
-              </Route>
-              <Route path="*" element={<PageNotFound />} />
-            </Routes>
+                  <Route path="Cities" element={<CityList />} />
+                  <Route path="cities/:id" element={<City />} />
+                  <Route path="Countries" element={<CountryList />} />
+                  <Route path="Form" element={<Form />} />
+                </Route>
+                <Route path="*" element={<PageNotFound />} />
+              </Routes>
+            </Suspense>
           </BrowserRouter>
         </CitiesProvider>
       </Authcontext>
--- src/Contexts/ContextCities.jsx
+++ src/Contexts/ContextCities.jsx
@@ -1,4 +1,10 @@
-import { useEffect, createContext, useContext, useReducer } from "react";
+import {
+  useEffect,
+  createContext,
+  useContext,
+  useReducer,
+  useCallback,
+} from "react";
 const Base_URL = "http://localhost:9000";
 
 const ContextCities = createContext();
@@ -80,20 +86,23 @@ const CitiesProvider = ({ children }) => {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
-    if (Number(id) === currentCity.id) return;
-    dispatch({ type: "loading" });
-    try {
-      const res = await fetch(`${Base_URL}/cities/${id}`);
-      const data = await res.json();
-      dispatch({ type: "city/loaded", payload: data });
-    } catch {
-      dispatch({
-        type: "rejected",
-        payload: "There is an error loading city",
-      });
-    }
-  }
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (Number(id) === currentCity.id) return;
+      dispatch({ type: "loading" });
+      try {
+        const res = await fetch(`${Base_URL}/cities/${id}`);
+        const data = await res.json();
+        dispatch({ type: "city/loaded", payload: data });
+      } catch {
+        dispatch({
+          type: "rejected",
+          payload: "There is an error loading city",
+        });
+      }
+    },
+    [currentCity.id]
+  );
 
   async function createCity(newCity) {
     dispatch({ type: "loading" });
